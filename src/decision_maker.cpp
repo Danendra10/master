08499235f72c44cde82b7181eadcffa4a0636214
Azzam Wildan <wildan.205024@mhs.its.ac.id>
2022-09-30 02:32:56 +0700

    fix data and ret

--- src/decision_maker.cpp
+++ src/decision_maker.cpp
@@ -31,16 +31,27 @@ uint8_t me_manual;
 
 // Multirole data
 gk_data_t gk_data;
+gk_ret_t gk_ret;
 att_data_t att_data;
 
 // Data sintetis sementara
 uint8_t robot_num = 1;
-uint8_t robot_role = 2;
+uint8_t robot_role = 1;
 uint8_t robot_num_bin[6] = {0b00000, 0b00001, 0b00010, 0b00100, 0b01000, 0b100000};
 
 // MS
 uint8_t game_status;
 uint8_t robot_action;
+
+void init_default_var()
+{
+    gk_data.robot_x[1] = 400;
+    gk_data.robot_y[1] = 0;
+    gk_data.robot_th[1] = 90;
+    gk_ret.vel_x_gain = 1;
+    gk_ret.vel_y_gain = 1;
+    gk_ret.vel_th_gain = 1;
+}
 
 void cllbck_pc2bs(const comm::mc_inConstPtr &msg)
 {
@@ -83,13 +94,19 @@ void game_process()
     switch (robot_role)
     {
     case 1:
+        // gk_data.game_status = 'K';
         gk_data.robot_num = robot_num;
         gk_data.robot_x[1] = 12;
         gk_data.robot_y[1] = 32;
         gk_data.ball_x = 123;
-        gk_data.ball_y = 234;
+        gk_data.ball_y = 300;
         gk_data.game_status = game_status;
-        gk_preparation(&gk_data, &robot_action);
+        gk_run(&gk_data, &gk_ret);
+
+        printf("ret: %d %d %d\n", gk_ret.act_type, gk_ret.target_x, gk_ret.target_y);
+
+        // Ret send to Motion Control node
+
         break;
     case 2:
         att_data.robot_num = robot_num;
@@ -99,6 +116,9 @@ void game_process()
         att_data.ball_y = 234;
         att_data.game_status = game_status;
         att_run(&att_data, &robot_action);
+
+        // Ret send to Motion Control node
+
         break;
     }
 
@@ -107,41 +127,41 @@ void game_process()
 
 void cllbck_main(const ros::TimerEvent &)
 {
-    static uint8_t prev_BS_cmd = 0;
-    static uint8_t prev_prev_BS_cmd = 0;
-    if (BS_normal)
-    {
-        // Prep..
-        if (prev_BS_cmd != BS_cmd && BS_cmd != 's' && BS_cmd != 'S')
-        {
-            game_status = BS_cmd;
-        }
+    // static uint8_t prev_BS_cmd = 0;
+    // static uint8_t prev_prev_BS_cmd = 0;
+    // if (BS_normal)
+    // {
+    //     // Prep..
+    //     if (prev_BS_cmd != BS_cmd && BS_cmd != 's' && BS_cmd != 'S')
+    //     {
+    //         game_status = BS_cmd;
+    //     }
 
-        // Start
-        if (prev_BS_cmd != BS_cmd && BS_cmd == 's')
-        {
-            // Safety tambahan ketika belum prep
-            if (game_status > 0 && game_status <= 127)
-                game_status += 128;
-        }
+    //     // Start
+    //     if (prev_BS_cmd != BS_cmd && BS_cmd == 's')
+    //     {
+    //         // Safety tambahan ketika belum prep
+    //         if (game_status > 0 && game_status <= 127)
+    //             game_status += 128;
+    //     }
 
-        // Stop
-        if (prev_BS_cmd != BS_cmd && BS_cmd == 'S')
-        {
-            game_status = 0;
-        }
-        prev_prev_BS_cmd = prev_BS_cmd;
-        prev_BS_cmd = BS_cmd;
-    }
-    else
-    {
-        if (me_manual == robot_num)
-        {
-            // ....
-            printf("Aku manual..\n");
-        }
-    }
-    // game_status = 'K' + 128;
+    //     // Stop
+    //     if (prev_BS_cmd != BS_cmd && BS_cmd == 'S')
+    //     {
+    //         game_status = 0;
+    //     }
+    //     prev_prev_BS_cmd = prev_BS_cmd;
+    //     prev_BS_cmd = BS_cmd;
+    // }
+    // else
+    // {
+    //     if (me_manual == robot_num)
+    //     {
+    //         // ....
+    //         printf("Aku manual..\n");
+    //     }
+    // }
+    game_status = 'K';
     // printf("Status game: %d\n", game_status);
     game_process();
 }
@@ -151,6 +171,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "decision_maker");
     ros::NodeHandle NH;
     ros::MultiThreadedSpinner spinner(2); // Sementara hanya punya 2 callback
+
+    init_default_var();
 
     ros::Subscriber sub_pc2bs = NH.subscribe("bs2pc", 10, cllbck_pc2bs);
 
